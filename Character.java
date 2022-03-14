0adf5cf2db2eabc4d6790fcb7eb9745f26d7e16f
S1thK3nny <62566066+S1thK3nny@users.noreply.github.com>
2022-03-14 18:22:59 +0100

    Add files via upload

---
+++ Character.java
@@ -0,0 +1,158 @@
+import java.lang.Math;
+
+public class Character {
+    //Default values START
+    private String name = "Default";
+    private int damage = 100;
+    private double attackspeed = 1.0;
+    private int life = 500;
+    private boolean forcesensitive = false;
+    private double tempattackdamage = 0;
+    private int finalattackdamage = 0; //Muss 0 sein, falls man sich heilt, dann darf man ja kein Schaden abziehen. UPDATE: Klappt wohl trotzdem nicht, huh. Egal, setze einfach den finalattackdamage bei dem Typen selber auf 0
+    
+    private String attack1 = "Melee";
+    private String attack2 = "Kick";
+    private String attack3 = "Bite";
+    private String attack4 = "Spit";
+    private String[] attacks = {attack1, attack2, attack3, attack4};
+    private String type = "Damage";
+    
+    private int attack = 0;
+    private int BurnCounter = 0;
+    //Default values STOP
+    
+    
+    //Getter START
+    public String getName() {
+        return name;
+    }
+    
+    public int getLife() {
+        return life;
+    }
+    
+    public int getDamage() {
+        return damage;
+    }
+    
+    public String[] getAttacks() {
+        return attacks;
+    }
+    
+    public double getTempAttackDamage() {
+        return tempattackdamage;
+    }
+    
+    public int getFinalAttackDamage() {
+        return finalattackdamage;
+    }
+    
+    public String getType() {
+        return type;
+    }
+    
+    public double getAttackSpeed() {
+        return attackspeed;
+    }
+    
+    public boolean getForceSensitive() {
+        return forcesensitive;
+    }
+    
+    public int getBurnCounter() {
+        return BurnCounter;
+    }
+    //Getter STOP
+    
+    
+    public void printStats() { //Printet Character Stats
+        System.out.println("\nName: " + getName() + "\nLife: " + getLife() + "\nDamage: " + getDamage() + "\nAttack speed: " + getAttackSpeed() + "\nForce sensitive: " + getForceSensitive());
+    }
+    
+    
+    
+    //Setters START
+    public void setStats(String name, String tier, boolean forcesensitive) {
+        this.name = name;
+        this.forcesensitive = forcesensitive;
+        if(tier.equals("TIER1")) {
+            this.life = 0 + (int)(Math.random() * ((1100 - 0) + 1)); //Danke für die Random Idee, Jan! :D
+            this.damage = 0 + (int)(Math.random() * ((250 - 0) + 1));
+            this.attackspeed = 1.0 + (Math.random() * (1.35-1.0));
+        }
+        else if(tier.equals("TIER2")) {
+            this.life = 900 + (int)(Math.random() * ((2100 - 900) + 1));
+            this.damage = 250 + (int)(Math.random() * ((500 - 250) + 1));
+            this.attackspeed = 1.15 + (Math.random() * (1.6-1.15));
+        }
+        else if(tier.equals("TIER3")) {
+            this.life = 1900 + (int)(Math.random() * ((3000 - 1900) + 1));
+            this.damage = 500 + (int)(Math.random() * ((750 - 500) + 1));
+            this.attackspeed = 1.45 + (Math.random() * (2-1.45));
+        }
+
+        else {
+            this.life = life;
+            this.damage = damage;
+            this.attackspeed = attackspeed;
+        }
+    }
+    
+    public void setLife(int life) { //Wird benötigt für die Attacken
+        this.life = life;
+    }
+    
+    //Attack Section
+    
+    public void setAttacks(String attack1, String attack2, String attack3, String attack4) {
+        this.attacks[0] = attack1;
+        this.attacks[1] = attack2;
+        this.attacks[2] = attack3;
+        this.attacks[3] = attack4;
+    }
+
+    public void setCurrentAttack(int attack) {
+        this.attack = attack;
+    }
+    
+    public void setFinalAttackDamage(int finalattackdamage) {
+        this.finalattackdamage = finalattackdamage;
+    }
+    
+    public void setBurnCounter(int BurnCounter) {
+        this.BurnCounter = BurnCounter;
+    }
+    //Setters STOP
+    
+
+
+    public void Attacks() {
+        System.out.println(getName() + " benutzt " + getAttacks()[attack-1]);
+        if(getAttacks()[attack-1].equals("Lichtschwert Angriff")) { //Again, muss -1 sein, da es ein Array ist. Wird pro Runde neu generiert, da Attacks() in der Schleife neu ausgeführt wird
+            type = "Damage";
+            double rndm = 0.8 + (Math.random() * (1.2-0.8));
+            tempattackdamage = damage * rndm;
+            finalattackdamage = (int)tempattackdamage;
+        }
+        else if(getAttacks()[attack-1].equals("Stimpacks") || getAttacks()[attack-1].equals("Machtheilung")) {
+            type = "Heal";
+            setLife(getLife()+250);
+            setFinalAttackDamage(0); //Wenn dies nicht der Fall ist, dann fügt der Spieler Schaden hinzu. Zu OP
+        }
+        else if(getAttacks()[attack-1].contains("schild") || getAttacks()[attack-1].equals("Abwehr")) {
+            type = "Shield";
+            setFinalAttackDamage(0); //Wenn dies nicht der Fall ist, dann fügt der Spieler Schaden hinzu. Zu OP
+        }
+        else if(getAttacks()[attack-1].equals("Flammenwerfer")) {
+            type = "Burn";
+            BurnCounter = 4;
+            tempattackdamage = damage * 1.0;
+            finalattackdamage = (int)tempattackdamage;
+        }
+        else { //Ist immer nur mal 1
+            type = "Damage";
+            tempattackdamage = damage * 1.0;
+            finalattackdamage = (int)tempattackdamage;
+        }
+    }
+}
---
+++ CharacterCreation.java
@@ -0,0 +1,272 @@
+import java.util.Scanner;
+
+public class CharacterCreation { //Our work is never over. ~Daft Punk
+    public void setPlayers(int p1, int p2) {
+        //Character Creation START
+        Character player1 = new Character();
+        Character player2 = new Character();
+        
+        Character LukeSkywalker = new Character();
+        Character DarthVader = new Character();
+        Character Palpatine = new Character();
+        Character RahmKota = new Character();
+        Character LeiaOrgana = new Character();
+        Character HanSolo = new Character();
+        Character Chewbacca = new Character();
+        Character BobaFett = new Character();
+        Character Stormtrooper = new Character();
+        Character RebelSoldier = new Character();
+        Character MilitiaSoldier = new Character();
+        //Character Creation STOP
+        
+        
+        
+        //Character Setters START
+        LukeSkywalker.setStats("Luke Skywalker", "TIER3", true); //Name, TIER, forcesensitive
+        DarthVader.setStats("Darth Vader", "TIER3", true);
+        Palpatine.setStats("Emperor Palpatine", "TIER3", true);
+        RahmKota.setStats("General Rahm Kota", "TIER3", true);
+        LeiaOrgana.setStats("Leia Organa", "TIER2", false);
+        HanSolo.setStats("Han Solo", "TIER2", false);
+        Chewbacca.setStats("Chewbacca", "TIER2", false);
+        BobaFett.setStats("Boba Fett", "TIER2", false);
+        Stormtrooper.setStats("Stormtrooper", "TIER1", false);
+        RebelSoldier.setStats("Rebel Soldier", "TIER1", false);
+        MilitiaSoldier.setStats("Kota Militia Soldier", "TIER1", false);
+        
+        LukeSkywalker.setAttacks("Lichtschwert Angriff", "Machtschub", "Abwehr", "Machtheilung");
+        DarthVader.setAttacks("Lichtschwert Angriff", "Machtschub", "Machtwürgung", "Lichtschwert Wurf");
+        Palpatine.setAttacks("Lichtschwert Angriff", "Machtblitz", "Machtwurf", "Dunkle Aura");
+        RahmKota.setAttacks("Lichtschwert Angriff", "Militia Unterstützung", "Machtschub", "Machtschild");
+        LeiaOrgana.setAttacks("Blaster Angriff", "Blaster Schnellfeuer", "Hoffnung", "Stimpacks");
+        HanSolo.setAttacks("Blaster Angriff", "Genauigkeit", "Sprengsatz", "Scharfschützenschuss");
+        //Chewbacca.setAttacks();
+        BobaFett.setAttacks("Blaster Angriff", "Flammenwerfer", "Handgelenkrakete", "Detpack");
+        //Stormtrooper.setAttacks();
+        //RebelSoldier.setAttacks();        
+        //MilitiaSoldier.setAttacks();
+        //Character Setters STOP
+        
+        
+        
+        //Switch START
+        switch(p1) {
+            case 1:
+                player1 = LukeSkywalker;
+                break;
+            case 2:
+                player1 = DarthVader;
+                break;
+            case 3:
+                player1 = Palpatine;
+                break;
+            case 4:
+                player1 = RahmKota;
+                break;
+            case 5:
+                player1 = LeiaOrgana;
+                break;
+            case 6:
+                player1 = HanSolo;
+                break;
+            case 7:
+                player1 = Chewbacca;
+                break;
+            case 8:
+                player1 = BobaFett;
+                break;
+            case 9:
+                player1 = Stormtrooper;
+                break;
+            case 10:
+                player1 = RebelSoldier;
+                break;
+            case 11:
+                player1 = MilitiaSoldier;
+                break;
+            default:
+                System.out.println("Wichtiger Fehler! Was ist passiert?");
+                break;
+        }
+        player1.printStats();
+        switch(p2) {
+            case 1:
+                player2 = LukeSkywalker;
+                break;
+            case 2:
+                player2 = DarthVader;
+                break;
+            case 3:
+                player2 = Palpatine;
+                break;
+            case 4:
+                player2 = RahmKota;
+                break;
+            case 5:
+                player2 = LeiaOrgana;
+                break;
+            case 6:
+                player2 = HanSolo;
+                break;
+            case 7:
+                player2 = Chewbacca;
+                break;
+            case 8:
+                player2 = BobaFett;
+                break;
+            case 9:
+                player2 = Stormtrooper;
+                break;
+            case 10:
+                player2 = RebelSoldier;
+                break;
+            case 11:
+                player2 = MilitiaSoldier;
+                break;
+            default:
+                System.out.println("Wichtiger Fehler! Was ist passiert?");
+                break;
+        }
+        player2.printStats();
+        //Switch STOP
+        
+        
+        int p1attack = 1; //MUSS AUSSERHALB DER WHILE SCHLEIFE SEIN!!! 
+        int p2attack = 1;
+        
+        //Kampf an sich START
+        while(true) {
+            Scanner attackinput = new Scanner(System.in);
+            if(player1.getAttackSpeed()>player2.getAttackSpeed()) { //Spieler 1 fängt an
+                    checkBurn(player2, player1, p2attack, p1attack);
+                    System.out.println("\nSpieler 1, wähle deine Attacke:");
+                    for(int i = 0; i<player1.getAttacks().length; i++) { //Je nach Anzahl der Attacken, meist 4
+                        System.out.println((i+1) + ") " + player1.getAttacks()[i]); //Printet z.b. 1) Melee
+                    }
+                    p1attack = attackinput.nextInt();
+                    player1.setCurrentAttack(p1attack); //Geht wieder rüber zu class "Character", setzt vorherige input Variable "p1attack" ein.
+                    player1.Attacks(); //setzt die damage values
+                    
+                    
+                    if(player2.getAttacks()[p2attack-1].contains("schild") || player2.getAttacks()[p2attack-1].equals("Abwehr")) { //MUSS VOR SETLIFE SEIN!!! Sonst klappt es nicht. WAG ES JA NICHT WIEDER DEN CODE HIER ZU ZWEIFELN; SONST KRIEGT DER SPIELER SCHADEN
+                        player1.setFinalAttackDamage(0);   
+                    }
+                    
+                    
+                    player2.setLife(player2.getLife()-player1.getFinalAttackDamage()); //Fügt den Schaden hinzu
+                    AttackOutput(player1, player2); //Printet ob man sich heilt oder ob man Schaden gemacht hat und wie viel
+                    
+                    if(player2.getLife()<0) { //Wenn Spieler 2 0 HP hat
+                        System.out.println("\nGlückwunsch, Spieler 1, du hast gewonnen!");
+                        break;
+                    }
+                    
+                    
+                    checkBurn(player1, player2, p1attack, p2attack);
+                    System.out.println("\nSpieler 2, wähle deine Attacke:");
+                    for(int i = 0; i<player2.getAttacks().length; i++) { //Je nach Anzahl der Attacken, meist 4
+                        System.out.println((i+1) + ") " + player2.getAttacks()[i]); //Printet z.b. 1) Melee
+                    }
+                    p2attack = attackinput.nextInt();
+                    player2.setCurrentAttack(p2attack); //Geht wieder rüber zu class "Character", setzt vorherige input Variable "p2attack" ein.
+                    player2.Attacks(); //setzt die damage values
+                    
+                    
+                    if(player1.getAttacks()[p1attack-1].contains("schild") || player1.getAttacks()[p1attack-1].equals("Abwehr")) {
+                        player2.setFinalAttackDamage(0);   
+                    }
+                    
+                    
+                    player1.setLife(player1.getLife()-player2.getFinalAttackDamage()); //Fügt den Schaden hinzu
+                    AttackOutput(player2, player1);
+                    
+                    if(player1.getLife()<0) { //Wenn Spieler 1 0 HP hat
+                        System.out.println("\nGlückwunsch, Spieler 2, du hast gewonnen!");
+                        break;
+                    }
+                }
+                
+                
+                else {
+                    checkBurn(player1, player2, p1attack, p2attack);
+                    System.out.println("\nSpieler 2, wähle deine Attacke:");
+                    for(int i = 0; i<player2.getAttacks().length; i++) {
+                        System.out.println((i+1) + ") " + player2.getAttacks()[i]);
+                    }
+                    p2attack = attackinput.nextInt();
+                    player2.setCurrentAttack(p2attack);
+                    player2.Attacks();
+                    
+                    if(player1.getAttacks()[p1attack-1].contains("schild") || player1.getAttacks()[p1attack-1].equals("Abwehr")) {
+                        player2.setFinalAttackDamage(0);   
+                    }
+                    
+                    
+                    player1.setLife(player1.getLife()-player2.getFinalAttackDamage());
+                    AttackOutput(player2, player1);
+                    
+                    if(player1.getLife()<0) {
+                        System.out.println("\nGlückwunsch, Spieler 2, du hast gewonnen!");
+                        break;
+                    }
+                    
+                    
+                    checkBurn(player2, player1, p2attack, p1attack);
+                    System.out.println("\nSpieler 1, wähle deine Attacke:");
+                    for(int i = 0; i<player1.getAttacks().length; i++) {
+                        System.out.println((i+1) + ") " + player1.getAttacks()[i]);
+                    }
+                    p1attack = attackinput.nextInt();
+                    player1.setCurrentAttack(p1attack);
+                    player1.Attacks();
+                    
+                    if(player2.getAttacks()[p2attack-1].contains("schild") || player2.getAttacks()[p2attack-1].equals("Abwehr")) {
+                        player1.setFinalAttackDamage(0);   
+                    }
+                    
+                    
+                    player2.setLife(player2.getLife()-player1.getFinalAttackDamage());
+                    AttackOutput(player1, player2);
+                    
+                    if(player2.getLife()<0) {
+                        System.out.println("\nGlückwunsch, Spieler 1, du hast gewonnen!");
+                        break;
+                    }
+                }
+        }
+        //Kampf an sich STOP
+    }
+    
+    public void AttackOutput(Character input1, Character input2) {
+        if(input1.getType().equals("Heal")) { //Falls die Attacke eine Heilung war.
+                        System.out.println("\n" + input1.getName() + " hat 250 Leben geheilt und hat somit jetzt " + input1.getLife() + " Leben.");
+                    }
+                else if (input1.getType().equals("Shield")) { //Falls die Attacke blockt/abwehrt
+                        System.out.println("\n" + input1.getName() + " ist für die nächste Runde geschützt.");
+                }
+                else {
+                        if(input1.getFinalAttackDamage()>input1.getDamage()) { //Falls es mehr Schaden macht, als dein Character kann (dank der Attacke)
+                        System.out.println("\n" + input1.getName() + " greift " + input2.getName() + " an und fügt " + input1.getFinalAttackDamage() + " Schaden hinzu, " + (input1.getFinalAttackDamage()-input1.getDamage()) + " Schaden mehr als es sollte. " + input2.getName() + " hat noch " + input2.getLife() + " Leben übrig.");
+                        }
+                        else { //Falls es weniger Schaden macht, als dein Character kann ("dank" der Attacke)
+                        System.out.println("\n" + input1.getName() + " greift " + input2.getName() + " an und fügt " + input1.getFinalAttackDamage() + " Schaden hinzu, " + (input1.getDamage()-input1.getFinalAttackDamage()) + " Schaden weniger als es sollte. " + input2.getName() + " hat noch " + input2.getLife() + " Leben übrig.");
+                        }
+                    }
+    }
+    
+    public void checkBurn(Character input1, Character input2, int p1attackinput, int p2attackinput) { //input1 = Brandverursacher, input2 = Opfer, p1attackinput = p1attack, p2attackinput = p2attack
+        if(input1.getBurnCounter()>0) { //Wenn der Brandschaden noch aktiv ist
+            input1.setBurnCounter(input1.getBurnCounter()-1); //Eins pro Runde runterstellen
+            if(input2.getAttacks()[p2attackinput-1].contains("schild") || input2.getAttacks()[p2attackinput-1].equals("Abwehr")) { //Falls Shield type aktiv ist
+                        System.out.println("\n" + input2.getName() + " hat den Brandschaden blockiert. Noch " + input1.getBurnCounter() + " Runden verbleibend bis der Brandschaden aufhört.");
+                }
+            else {
+                input2.setLife(input2.getLife()-50);
+                System.out.println("\n" + input2.getName() + " hat 50 Leben an Brandschaden verloren. Noch " + input1.getBurnCounter() + " Runden verbleibend bis der Brandschaden aufhört.");
+            }
+        }
+        else {
+            //Deine Mutter LMAOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+        }
+    }
+}
